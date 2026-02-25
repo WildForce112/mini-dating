6a73c5ce00b2a28f942401c13bdf6ed2ff5b189b
WildForce112 <81035327+WildForce112@users.noreply.github.com>
2026-02-25 11:23:33 +0700

    feat: added browserrouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,45 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router'
+import { getStoredUser } from './auth';
 import './index.css'
 import App from './App.jsx'
+import DateGrid from './components/DateGrid.jsx'
+import CreateProfile from './pages/Register.jsx'
+import Login from './pages/Login.jsx';
+import API from './api.js';
+
+function Root(){
+  const [profiles, setProfiles] = useState([]);
+  const [user, setUser] = useState(getStoredUser());
+  const fetchProfiles = async () => {
+    const res = await API.get("/profiles");
+    setProfiles(res.data);
+  }
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: user ? <App user={user} profiles={profiles} fetchProfiles={fetchProfiles} setUser={setUser}/> : <Navigate to="/login" replace />,
+    },
+    {
+      path: "/login",
+      element: <Login profiles={profiles} fetchProfiles={fetchProfiles} setUser={setUser}/>,
+    },
+    {
+      path: "/create",
+      element: <CreateProfile setProfiles={setProfiles}/>,
+    },
+    {
+      path: "/plan",
+      element: user ? <DateGrid /> : <Navigate to="/login" replace />,
+    }
+  ])
+  return <RouterProvider router={router} />;
+}
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 )
