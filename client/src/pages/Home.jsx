62484d1178f4013529b684c569f52eb44fd112da
WildForce112 <81035327+WildForce112@users.noreply.github.com>
2026-02-26 07:40:36 +0700

    feat: added listing of people you matched

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,22 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import API from "../api"
 import { useNavigate } from 'react-router';
 import { setStoredUser } from '../auth';
 import Profiles from '../components/Profiles';
 
-export default function Home({ user, profiles, fetchProfiles, setUser }){
+export default function Home({ user, setUser }){
   const navigate = useNavigate()
+  const [matches, setMatches] = useState([]);
+  const [notMatched, setNotMatched] = useState([]);
+  const fetchDiscovery = async() => {
+    const res = await API.get(`/discover/${user}`);
+    setMatches(res.data.matches);
+    setNotMatched(res.data.notMatched);
+  }
   useEffect(() => {
-    fetchProfiles();
+    fetchDiscovery();
   }, []);
+  console.log(matches);
   const handleLike = async (targetID) => {
     try {
       const res = await API.post("/like", {
@@ -16,6 +24,7 @@ export default function Home({ user, profiles, fetchProfiles, setUser }){
         toID: targetID,
       });
       alert(res.data?.message);
+      await fetchDiscovery();
     } catch (err) {
       alert(err.response?.data?.message);
     }
@@ -30,17 +39,28 @@ export default function Home({ user, profiles, fetchProfiles, setUser }){
       display: "flex",
       width: "100vw",
     }}>
-      <h1 style={{
-        width: "30vw",
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "gray",
-        margin: "0",
-        position: "fixed",
-      }}>Mini Dating App</h1>
-      <Profiles profiles={profiles} buttonLabel="Like" handleFunction={handleLike} width="70vw"/>
+      <div style={{
+          width: "30vw",
+          height: "100vh",
+          display: "flex",
+          paddingLeft: "1vw",
+          paddingRight: "1vw",
+          paddingTop: "1vh",
+          paddingBottom: "1vh",
+          backgroundColor: "gray",
+          margin: "0",
+          position: "fixed",
+          textAlign: "left",
+          flexDirection: "column",
+        }}>
+        <h1 >Mini Dating App</h1>
+        {matches.map((p) => (
+          <div key={p.id}>
+            <h3>{p.name} ({p.age}y.o)</h3>
+          </div>
+        ))}
+      </div>
+      <Profiles profiles={notMatched} buttonLabel="Like" handleFunction={handleLike} width="70vw"/>
       <button style={{
         position: "absolute",
         top: 0,
